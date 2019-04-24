0b71f274aa9239a590a21378deb08baa5448685a
VirtualLindsey <tapircode@gmail.com>
2019-04-23 21:13:19 -0700

    finishing #3

---
+++ arrays/ex3.rb
@@ -0,0 +1,8 @@
+#How do you return the word "example" from the following array?
+arr = [["test", "hello", "world"],["example", "mem"]]
+
+def find_example(arr)
+  arr[1][0]
+end
+
+find_example(arr)
