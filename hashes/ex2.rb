5ec61a9871aea3c7d8e14dff98e7937b810faf0b
VirtualLindsey <tapircode@gmail.com>
2019-04-25 14:51:41 -0700

    adding flatten method, building ex2

---
+++ hashes/ex2.rb
@@ -0,0 +1,12 @@
+#Look at Ruby's merge method. Notice that it has two versions.
+#What is the difference between merge and merge!?
+#Write a program that uses both and illustrate the differences.
+
+cats_we_know = {
+  Puddinghead: "sleepy", Peteycat: "hungry", Meowx: "angry", George: "demonic",
+  Angel: "cuddly"
+}
+
+famous_cats = {
+  Garfield: "lazy", Salem: "clever", Luna: "bossy"
+}
